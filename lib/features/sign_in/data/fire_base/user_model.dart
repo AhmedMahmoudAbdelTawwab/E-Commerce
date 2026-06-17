079b848bce4ad66992cdd7ff3604bb69d09b1653
ahmed mahmoud <ahmedmahmous876234@gmail.com>
2026-06-17 11:03:38 +0300

    Add Firebase authentication and core dependencies, and configure platform support

---
+++ lib/features/sign_in/data/fire_base/user_model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class UserModel {
+  String naem;
+  String password;
+  String email;
+  String userImage;
+  String id;
+  UserModel({
+    required this.naem,
+    required this.password,
+    required this.email,
+    required this.userImage,
+    required this.id,
+  });
+}
